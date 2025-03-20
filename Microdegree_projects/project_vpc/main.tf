73b97c06a6a402c5af3852ca16d7f83c1b67aa2d
Git-Prajwal-GH <prajwalhanchinal917@gmail.com>
2025-03-20 16:14:50 +0530

    Update main.tf

--- Microdegree_projects/project_vpc/main.tf
+++ Microdegree_projects/project_vpc/main.tf
@@ -1,3 +1,13 @@
-provider {
-    
+provider "aws"{
+      region="us-east-1"
+      access_key="*****"
+      secret_key="******"
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
+
+        tags = {
+            Name = "test-vpc"
+        }
 }
