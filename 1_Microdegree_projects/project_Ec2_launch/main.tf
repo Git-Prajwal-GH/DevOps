574244b77dafeba98387a4aecdd67f948cf0f242
Git-Prajwal-GH <prajwalhanchinal917@gmail.com>
2025-04-19 12:28:34 +0530

    edit the project

---
+++ 1_Microdegree_projects/project_Ec2_launch/main.tf
@@ -0,0 +1,30 @@
+# LAUNCHING EC2 INSTANCE USING TERRAFORUM SCRIPT
+provider "aws" {
+    region = "us-east-1"
+    access_key = "*****"
+    secret_key = "****"
+}
+
+resource "aws_instance" "admin" {
+        ami = "ami-0c4e709339fa8521a" 
+        instance_type = "t2.medium"
+        security_groups = [ "default" ]
+        key_name = "terraform"
+
+
+     root_block_device {
+        volume_size = 25
+        volume_type = "gp3"
+        delete_on_termination = true
+
+     }
+
+     tags = {
+        Name ="Admin-server"
+     }   
+}
+
+
+output "publicIP" {
+            value = aws_instance.admin.public_ip
+}
