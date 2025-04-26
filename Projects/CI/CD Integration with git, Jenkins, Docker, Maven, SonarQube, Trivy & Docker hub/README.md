e16e10bda3f41b6abdd4d3277192a8b37f2d192d
Git-Prajwal-GH <prajwalhanchinal917@gmail.com>
2025-04-26 18:42:20 +0530

    updating

---
+++ Projects/CI/CD Integration with git, Jenkins, Docker, Maven, SonarQube, Trivy & Docker hub/README.md
@@ -0,0 +1,15 @@
+# CICD PROJECT 
+
+<img alt="Terraform" src="https://github.com/user-attachments/assets/054c74b4-45e9-42d9-8b25-83064accf134" width="800px">
+
+## CREATE EC2 INSTANCE AS FOLLOW : 
+
+<img alt="images" src="" width="600px">
+<img alt="images" src="" width="600px">
+<img alt="images" src="" width="600px">
+<img alt="images" src="" width="600px">
+
+- 
+- 
+- 
+- 
--- Projects/project_k8s/main.tf
+++ Projects/project_k8s/main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "admin" {
-        ami = "ami-084568db4383264d4"            # copy from AMI catalog which is displayed
-        instance_type = "t2.medium"              # choose "t2.medium" instance
-        security_groups = [ "default" ]          # set security group as default
-        key_name = "k8s"                   # Give keypair
-        root_block_device {
-        volume_size = 20                         # edit required storage capacity
-        volume_type = "gp3"
+        ami                   = " "            # copy from AMI catalog which is displayed
+        instance_type         = "t2.micro"              # choose "t2.medium" instance
+        security_groups       = [ "default" ]          # set security group as default
+        key_name              = "k8s"                   # Give keypair
+  root_block_device {
+        volume_size           = 20                         # edit required storage capacity
+        volume_type           = "gp3"
         delete_on_termination = true             # EBS will get auto-deleted affter destroying the instance
 
      }
